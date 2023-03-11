37df00fd807c75ffe3e72a3772658c427cee32bd
HelenaKhitrenko <helena.khitrenko@gmail.com>
2023-03-11 08:54:24 +0100

    summary

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,57 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const galleryMarkup = makeGalleryMarkup(galleryItems);
 
-console.log(galleryItems);
+addMarkupOfElement(gallery, galleryMarkup);
+
+let currentModalMarkup = "";
+
+gallery.addEventListener("click", showModal);
+
+function makeGalleryMarkup(items = {}) {
+  return items
+    .map(
+      ({ preview = "", original = "", description = "" }) => `
+      <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+         <img
+            class="gallery__image"
+             src="${preview}"
+             data-source="${original}"
+             alt="${description}"
+         />
+         </a>
+      </div>`
+    )
+    .join("");
+}
+
+function addMarkupOfElement(el, murkup) {
+  el.innerHTML = murkup;
+}
+
+function showModal(event) {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  currentModalMarkup = makeModalMarkup(event);
+  currentModalMarkup.show();
+}
+
+function makeModalMarkup(event) {
+  event.preventDefault();
+
+  const link = event.target.dataset.source;
+  const closeModalForEsc = (event) => {
+    if (event.key === "Escape") {
+      currentModalMarkup.close();
+    }
+  };
+
+  return basicLightbox.create(`<img src="${link}" width="800" height="600">`, {
+    onShow: () => window.addEventListener("keydown", closeModalForEsc),
+    onClose: () => window.removeEventListener("keydown", closeModalForEsc),
+  });
+}
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+const gallery = document.querySelector(".gallery");
+const galleryMarkup = makeGalleryMarkup(galleryItems);
 
-console.log(galleryItems);
+addMarkupOfElement(gallery, galleryMarkup);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+function makeGalleryMarkup(items = {}) {
+  return items
+    .map(
+      ({ preview = "", original = "", description = "" }) => `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+    )
+    .join("");
+}
+
+function addMarkupOfElement(el, murkup) {
+  el.innerHTML = murkup;
+}
